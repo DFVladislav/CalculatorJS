9199198c3a010fa6d2f4fec8c884c25283e9a509
DFedosov <fedya12346598@mail.ru>
2026-03-22 01:06:48 +0300

    Update mini-add

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 let firstNum = document.getElementById("firstNumber")
 let secondNum = document.getElementById("secondNumber")
 let resultAll = document.getElementById("resultAll")
+let later = document.getElementById("later")
+
+
 
 
 function getNumber() {
     return {
         a: parseFloat(firstNum.value),
-        b: parseFloat(secondNum.value)
+        b: parseFloat(secondNum.value),  
     }
 }
 
-function plus() {
+
+function plus() {       /
     let nums = getNumber()
     let result = nums.a + nums.b
-
-    resultAll.innerText = result
+    resultAll.innerText = `Ваш результат: ${result}`
     console.log(result)
 }
 
-function minus() {
+function minus() {                      
     let nums = getNumber()
     let result = nums.a - nums.b
 
-    resultAll.innerText = result
+    resultAll.innerText = `Ваш результат: ${result}`
     console.log(result)
 }
 
@@ -30,35 +33,39 @@ function multiplication() {
     let nums = getNumber()
     let result = nums.a * nums.b
 
-    resultAll.innerText = result
+    resultAll.innerText = `Ваш результат: ${result}`
     console.log(result)
 }
 
 function divisity() {
     let nums = getNumber()
     let result = nums.a / nums.b
-    if (nums.b === 0) {
+    if (nums.b === 0 || nums.a === 0) {
         resultAll.innerText = "0 нельзя"
     }
     else {
-        resultAll.innerText = result
+        resultAll.innerText = `Ваш результат: ${result}`
     }
     console.log(result)
 }
 function procent() {
-
     let nums = getNumber()
     let result = nums.a % nums.b
-
-    resultAll.innerText = result
+    if (nums.b === 0 || nums.a === 0) {
+        resultAll.innerText = "0 нельзя"
+    }
+    else {
+        resultAll.innerText = `Ваш результат: ${result}`
+    }
     console.log(result)
 }
 
 function stepen() {
     let nums = getNumber()
-    let result  = nums.a ** nums.b
+    let result = nums.a ** nums.b
 
-    resultAll.innerText =  result
+    resultAll.innerText = `Ваш результат: ${result}`
     console.log(result)
 }
+
 
